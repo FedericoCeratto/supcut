766b23209b0557b6b73868b8cb1a950d5d154e0f
Federico Ceratto <federico.ceratto@gmail.com>
2010-11-29 23:04:30 +0000

    setup.py configured

--- setup.py
+++ setup.py
@@ -1,18 +1,26 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.5'
+from supcut.supcut import __version__ as version
 
 setup(name='supcut',
       version=version,
       description="Simple unobtrusive Python continuous unit testing",
       long_description="""\
 A rather hassle-free tool to run nose based unit testing locally.""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        'Development Status :: 4 - Beta',
+      ],
       keywords='nose unit-testing',
       author='Federico Ceratto',
       author_email='federico.ceratto@gmail.com',
-      url='https://github.com/FedericoCeratto/supcu',
+      url='http://github.com/FedericoCeratto/supcut',
       license='GPLv3',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
@@ -20,7 +28,7 @@ A rather hassle-free tool to run nose based unit testing locally.""",
       install_requires=[
           # -*- Extra requirements: -*-
       ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+      entry_points={
+        'console_scripts': ['supcut = supcut.supcut:main'],
+      },
+)
